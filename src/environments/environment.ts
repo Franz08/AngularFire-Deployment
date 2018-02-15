0e7c5b9ee45ea003f407e0ecfdae259563331cb2
Franz08 <blicanel@yahoo.com>
2018-02-16 00:36:02 +0800

    Inital push

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  apiKey: "AIzaSyAMsVe2iRSdA0hmrM7V_Jv4P5b2R6AN-hU",
+    authDomain: "ngx-admin-807d4.firebaseapp.com",
+    databaseURL: "https://ngx-admin-807d4.firebaseio.com",
+    projectId: "ngx-admin-807d4",
+    storageBucket: "ngx-admin-807d4.appspot.com",
+    messagingSenderId: "392913403870"
+};
